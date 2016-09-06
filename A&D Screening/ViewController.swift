95db367944aef5716ea054fb9061000a8f9566b8
Andrew Stalone <ajsgolf27@aol.com>
2016-09-05 21:45:38 -0400

    Initial Commit

---
+++ A&D Screening/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A&D Screening
+//
+//  Created by Andrew Stallone on 9/5/16.
+//  Copyright © 2016 Cyndi Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
